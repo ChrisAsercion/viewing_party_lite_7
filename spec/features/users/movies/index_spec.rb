079643dd76b0b5654b99b4cc066a5ab61c1ec4fd
Chris Asercion <chris.asercion@gmail.com>
2023-08-23 15:00:10 -0600

    Adds stub request

--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -5,9 +5,18 @@ RSpec.describe "Users Movie index" do
   describe "index" do
     scenario "when I visit users/user_id/movies" do
       it "shows the top 20 movies" do
-        json_response = File.read()
+        stub_request(:get, "https://api.themoviedb.org/3/discover/movie?api_key=88c7be8577d6a96191637ecd1c0e4a1d").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v2.7.10'
+          }).
+        to_return(status: 200, body: "", headers: {})
 
-        stub_request(:get, "")
+        expect(page.status_code).to eq 200
+        expect(page).to have_content("Senator Bernard Sanders was found!")
+        expect(page).to have_content("SenSanders")
       end
     end
   end
