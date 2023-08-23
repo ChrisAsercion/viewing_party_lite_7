31bcd3f4fece326ea6d087719877cc8c170868f4
Chris Asercion <chris.asercion@gmail.com>
2023-08-23 13:37:48 -0600

    work

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,11 +1,11 @@
 class Users::MoviesController < ApplicationController
   def index
     #@facade = MovieFacade.new
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["api_key"] = "ed85c4caf218889cdedcbf633681d9f4"
-    end
+    conn = Faraday.new(url: "https://api.themoviedb.org") #do |faraday|
+      #faraday.headers["api_key"] = "ed85c4caf218889cdedcbf633681d9f4"
+    #end
     
-    response = conn.get("/3/discover/movie/11.json")
+    response = conn.get("/3/discover/movie/?api_key=#{ENV["movie_key"]}")
     require 'pry'; binding.pry
   end
 end
